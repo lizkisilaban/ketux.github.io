80aca5e32d4139c11da3125ea482fefcc1855d1a
lizkisilaban <94830737+lizkisilaban@users.noreply.github.com>
2023-06-17 21:44:35 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                // untuk membuat nama class warna sendiri
+                primary: '#ffffff',
+                dark: '#0f172a',
+                warna: '#0055BA',
+
+            }
+        },
+    },
+    plugins: [],
+}
